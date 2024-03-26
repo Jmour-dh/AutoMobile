cb328928ac7a1ad872ecd4152d3d1f0b1460505d
dev5 <125797700+Jmour-dh@users.noreply.github.com>
2024-03-26 11:43:40 +0100

    commetaires

--- frontend/src/utils/ProtectedRouteRegister.jsx
+++ frontend/src/utils/ProtectedRouteRegister.jsx
@@ -1,10 +1,15 @@
-// ProtectedRoute.jsx
+// Importation des modules nécessaires depuis React et react-router-dom
 import React from "react";
 import { Navigate } from "react-router-dom";
 
+// Composant de route protégée pour l'inscription
 function ProtectedRouteRegister({ children, allowedRoles }) {
+  // Récupération du token utilisateur depuis le stockage local
   const token = localStorage.getItem("userToken");
+  
+  // Vérifie si un token est présent, si oui, redirige vers la page d'accueil, sinon, rend le contenu enfant
   return token ? <Navigate to="/" /> : children;
 }
 
+// Exportation du composant ProtectedRouteRegister
 export default ProtectedRouteRegister;
